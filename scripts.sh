5c74ef41b9584016c963069fb2f9a417ef108ae1
chiyoi <chiyoi2140@outlook.com>
2023-10-24 12:11:00 +0800

    tidy

--- scripts.sh
+++ scripts.sh
@@ -4,9 +4,17 @@ usage() {
     pwd
     echo "Scripts:"
     echo "$0 tidy"
-    echo "    Go mod tidy."
+    echo "    Tidy go module."
     echo "$0 run"
     echo "    Run the main package."
+    echo "$0 pull"
+    echo "    Pull from git origin."
+    echo "$0 build"
+    echo "    Build docker image."
+    echo "$0 up"
+    echo "    Run in docker."
+    echo "$0 update"
+    echo "    Pull, Build and Up."
 }
 
 dev_env() {
@@ -25,13 +33,29 @@ run() {
     go run .
 }
 
+pull() {
+    git pull
+}
+
+build() {
+    docker build -t chiyoi/morph .
+}
+
+up() {
+    docker run -d --network=host --restart=on-failure:5 --name=morph chiyoi/morph
+}
+
+update() {
+    pull && build && up
+}
+
 if test -z "$1" -o -n "$(echo "$1" | grep -Ex '\-{0,2}h(elp)?')"; then
 usage
 exit
 fi
 
 case "$1" in
-tidy|run);;
+tidy|run|pull|build|up|update);;
 *)
 usage
 exit 1
